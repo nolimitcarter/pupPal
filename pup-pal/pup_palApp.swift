5beb25db0b18b7cfc8ce82e9831a3ddc37da55a8
nolimitcarter <carterthayer@protonmail.com>
2021-03-29 22:37:07 -0400

    Initial Commit

---
+++ pup-pal/pup_palApp.swift
@@ -0,0 +1,17 @@
+//
+//  pup_palApp.swift
+//  pup-pal
+//
+//  Created by root on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct pup_palApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pup-palTests/pup_palTests.swift
@@ -0,0 +1,33 @@
+//
+//  pup_palTests.swift
+//  pup-palTests
+//
+//  Created by root on 3/29/21.
+//
+
+import XCTest
+@testable import pup_pal
+
+class pup_palTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
